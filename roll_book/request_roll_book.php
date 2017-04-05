96dfd68ff19e1240070117e47396ec59956a8505
kaka <kaka@gmail.com>
2017-04-05 11:06:19 +0900

    0405

--- roll_book/request_roll_book.php
+++ roll_book/request_roll_book.php
@@ -19,14 +19,14 @@ exit;
 $catch_l_id = $_POST["l_id"];
 $catch_date = $_POST["date"];
 
-$catch_roll_book = mysqli_query($my_db, "SELECT user_name, a_id AS user_id, b_id AS roll_check
+$catch_roll_book = mysqli_query($my_db, "SELECT user_name, a_id AS user_id, b_id AS roll_check, nfc_code
                                          FROM User_table
                                          RIGHT OUTER JOIN (
-                                            SELECT A.user_id AS a_id, B.user_id AS b_id
+                                            SELECT A.user_id AS a_id, B.user_id AS b_id, B.nfc_code
                                             FROM (SELECT *
                                                   FROM Roll_Book
                                                   WHERE l_id = '" . $catch_l_id . "') AS A
-                                                  LEFT OUTER JOIN (SELECT l_id, check_date, user_id
+                                                  LEFT OUTER JOIN (SELECT *
                                                                    FROM Roll_Book_Check
                                                                    WHERE l_id = '" . $catch_l_id . "') AS B
 				                                  ON A.l_id = '" . $catch_l_id . 
@@ -40,7 +40,7 @@ $send_roll_book;
 
 while($data = mysqli_fetch_array($catch_roll_book)){
     
-    $send_roll_book .= $data['user_name'] . "∥" . $data['user_id']. "∥";
+    $send_roll_book .= $data['user_name'] . "∥" . $data['user_id']. "∥" . $data['nfc_code'] . "∥";
     if($data['roll_check'] == "")
     {
         $send_roll_book .= "M";
