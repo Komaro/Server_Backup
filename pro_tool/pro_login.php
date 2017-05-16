96bf3e23d874a24b7603ba3032596c4454e6c295
kaka <kaka@gmail.com>
2017-05-16 19:04:03 +0900

    0516

---
+++ pro_tool/pro_login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>pro Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    
+    <div class = "row">
+    <div class = "col-sm-4"></div>
+    <div class = "col-sm-4">
+        <h1 class = "text-center">WELCOM PROFESSOR</h1>
+            <form method = "post" action = "login_pass.php">
+            <div class = "box">
+                <input type = "text" class = "form-control" name = "id"/>
+                <br>
+                <input type = "password" class = "form-control" name = "pass" class = "iText"/>
+                <br>
+            </div>
+            <input type = "submit" class = "btn btn-default btn-block" value="LOGIN">
+        </form>
+    </div>
+    <div class = "col-sm-4"></div>
+    </div>
+</body>
+
+</html>
